6eacaa22bfcf043470a056a6d337f42f0b1cbfd9
Mike <mike@digitaldjpool.com>
2019-05-14 17:20:08 -0400

    add server, testing

---
+++ server.js
@@ -0,0 +1,14 @@
+// run this script by terminal window like this:
+// > node server.js
+// or node interactive window like this:
+// > require('server')
+var http = require('http');
+var fs = require("fs");
+
+http.createServer(function (request, response) {
+	fs.readFile("authcodepkce.html", function (err, data) {
+		response.writeHead(200, { 'Content-Type': 'text/html' });
+		response.write(data);
+		response.end();
+	});
+}).listen(3000);
